1526cdade056e5d492572d36b4acc661f46fa4c4
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2023-06-01 22:41:08 +0530

    Update 1_Performance.js

--- BigO/1_Performance.js
+++ BigO/1_Performance.js
@@ -5,17 +5,17 @@ let nemo = ["nemo"];
 let largeArray = new Array(100).fill("Nemo");
 
 function findNemo(arr) {
-    let t0 = performance.now();
-    console.log("t0 : ", t0);
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === "Nemo") {
-            console.log("Found NEMO!");
-        } else {
-            console.log("Not Found");
-        }
+  let t0 = performance.now();
+  console.log("t0 : ", t0);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "Nemo") {
+      console.log("Found NEMO!");
+    } else {
+      console.log("Not Found");
     }
-    let t1 = performance.now();
-    console.log("t1 : ", t1);
-    console.log("Total Time Taken : ", (t0 - t1));
+  }
+  let t1 = performance.now();
+  console.log("t1 : ", t1);
+  console.log("Total Time Taken : ", t0 - t1);
 }
-findNemo(largeArray)
+findNemo(largeArray);
